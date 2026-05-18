7094b3d5d67bd53c7c0276f0a46b378f0e035d4c
Arden28 <laudbouetoumoussa@gmail.com>
2026-05-18 16:09:22 +0300

    Updated the footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -181,7 +181,7 @@ export const Footer = () => {
               ].join(' ')}
               aria-label="Arden Bouet"
             >
-              Arden<br />Bouetoumoussa
+              Arden<br />Bouet
             </p>
 
             {/* Role + location */}
